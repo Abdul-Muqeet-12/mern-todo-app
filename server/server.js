0bf9c0dc4f7661839e418c1451ba990c4a88ed27
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-16 22:54:56 +0500

    Initial backend setup with MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(cors({ credentials: true }));
 
