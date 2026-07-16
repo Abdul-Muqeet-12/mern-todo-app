545c6b1625c91d1c90feff5de7a75a9c032d9f27
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-16 23:08:55 +0500

    Create User Model

---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
