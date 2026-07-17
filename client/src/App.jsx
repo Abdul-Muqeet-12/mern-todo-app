ab63711c6bd0450bd8cc06681a4a428a8698b42b
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-17 20:43:22 +0500

    Implement protected routes for authenticated users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Todo from "./pages/Todo";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Todo />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Todo />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
