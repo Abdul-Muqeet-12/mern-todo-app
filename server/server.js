9ae8cd545343ce97aeb03b4929cd03a21d6a9c78
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-18 11:01:49 +0500

    Prepare backend for production deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
